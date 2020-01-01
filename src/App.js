18926e0c5d7bd33943c0ab4bcefdc2d72fda0a08
Ying Lu <yinglu91@gmail.com>
2020-01-01 18:22:29 -0500

    tech list

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 
 import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -25,6 +26,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </>
