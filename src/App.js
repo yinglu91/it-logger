5de98db25de55ff51ba527ba9d00901199d84b8b
Ying Lu <yinglu91@gmail.com>
2020-01-01 18:02:37 -0500

    add tech modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+
+import AddTechModal from './components/techs/AddTechModal';
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -20,6 +23,8 @@ const App = () => {
       <div className='container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
         <Logs />
       </div>
     </>
--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const EditLogModal = () => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -20,19 +20,19 @@ const AddLogModal = () => {
   };
 
   return (
-    <div id='add-log-modal' className='modal' style={modalStyle}>
+    <div id='edit-log-modal' className='modal' style={modalStyle}>
       <div className='modal-content '>
         <h4>Enter System Log</h4>
         <div className='row'>
           <div className='input-field'>
             <input
               type='text'
-              id='message'
+              id='message1'
               name='message'
               value={message}
               onChange={e => setMessage(e.target.value)}
             />
-            <label htmlFor='message' className='active'>
+            <label htmlFor='message1' className='active'>
               Log Message
             </label>
           </div>
@@ -92,4 +92,4 @@ const modalStyle = {
   height: '75%'
 };
 
-export default AddLogModal;
+export default EditLogModal;
