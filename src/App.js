d850d17f84434621758ed963b13d01be4b3092a0
Ying Lu <yinglu91@gmail.com>
2020-01-07 21:14:09 -0500

    added redux and getLogs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -19,17 +22,19 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </>
+    <Provider store={store}>
+      <>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </>
+    </Provider>
   );
 };
 
