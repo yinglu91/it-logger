58d0a71484d6b041f3bdb5bdfbf19dd584584963
Ying Lu <yinglu91@gmail.com>
2020-01-08 21:39:04 -0500

    added tech reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import logReducer from './logReducer';
+import techReducer from './techReducer';
 
+// for all the resources
 export default combineReducers({
-  log: logReducer
+  log: logReducer,
+  tech: techReducer
 });
